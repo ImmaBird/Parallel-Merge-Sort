757345353058b8267b0c98f5c99dd174be12057d
ImmaBird <immapokeyew@gmail.com>
2018-08-03 17:06:26 -0400

    cleaned up

--- cuda/sort.cu
+++ cuda/sort.cu
@@ -3,54 +3,69 @@
 #include <time.h>
 #include "sort.cuh"
 
-#define SIZE 16000000
-#define THREADS_PER_BLOCK 512
-
+#define SIZE 10000000
 #define STARTRANGE 0
 #define ENDRANGE 100
 
-void insertionSortA(int *array, int a, int b);
+#define THREADS_PER_BLOCK 1024
 
-int comparator(const void *p, const void *q) 
-{
-    // Get the values at given addresses
-    int l = *(const int *)p;
-    int r = *(const int *)q;
+// flag if the prng has been seeded
+int randNotSeeded = 1;
 
-    return l-r;
-}
-
+// tests the gpu merge sort
 int main()
 {
-    // TODO: Breaks if using any array size that isn't a power of 2
-    // TODO: Also breaks if using an array size that is a power of 2 >= 2^20 :)
-    // TODO: Make the above two todos not happen
-    srand(time(0));
-
-    int *data;
-    data = getRandomArray(SIZE);
-    int *test = (int*)malloc(SIZE*sizeof(int));
-    memcpy(test, data, SIZE*sizeof(int));
-    qsort(test, SIZE, sizeof(int), comparator);
-
+    // variables to time the sort
     clock_t start, stop;
+
+    // the array to test our sort on
+    int *data = getRandomArray(SIZE);
+
+    // print the first 15 elements of the data
+    if (SIZE > 15)
+    {
+        printArray(data, 15);
+    }
+    else
+    {
+        printArray(data, SIZE);
+    }
+
+    // gets the right answer to compare too at the end
+    int *data_qsort = (int*)malloc(SIZE*sizeof(int));
+    memcpy(data_qsort, data, SIZE*sizeof(int));
+    qsort(data_qsort, SIZE, sizeof(int), comparator);
+
+    // runs the program and times it
     start = clock();
     mergeSort(data, SIZE);
     stop = clock();
+    
 
-    printf("\n\n");
-    double elapsed = ((double) (stop - start)) / CLOCKS_PER_SEC;
-    printf("Elapsed time: %.3fs\n", elapsed);
-
-
-    for (int i = 0; i < SIZE; i++) {
-        if (data[i] != test[i]) {
-            printf("Broken :( %d\n", i);
-            break;
-        }
+    // print the first 15 elements of the hopefully sorted data array
+    printf("\n");
+    if (SIZE > 15)
+    {
+        printArray(data_qsort, 15);
     }
-    printArray(test, 15);
-    printArray(data, 15);
+    else
+    {
+        printArray(data_qsort, SIZE);
+    }
+
+    // prints the first 15 elements of the sorted array
+    if (SIZE > 15)
+    {
+        printArray(data, 15);
+    }
+    else
+    {
+        printArray(data, SIZE);
+    }
+
+    // print elapsed time
+    double elapsed = ((double) (stop - start)) / CLOCKS_PER_SEC;
+    printf("Elapsed time: %.3fs\n\n", elapsed);
 
     // Cleanup
     free(data);
@@ -58,26 +73,22 @@ int main()
     return 0;
 }
 
+// parallel merge sort using a GPU
 void mergeSort(int *array, int arraySize)
 {
-    cudaError_t err;
-
     // Make array in gpu memory
     int *d_array;
-    err = cudaMalloc((void **)&d_array, arraySize*sizeof(int));
-    //printf("malloc: %s\n", cudaGetErrorString(err));
-    err = cudaMemcpy(d_array, array, arraySize*sizeof(int), cudaMemcpyHostToDevice);
-    //printf("cpy: %s\n", cudaGetErrorString(err));
+    cudaMalloc((void **)&d_array, arraySize*sizeof(int));
+    cudaMemcpy(d_array, array, arraySize*sizeof(int), cudaMemcpyHostToDevice);
 
     int chunkSize = 2;
     int chunks = arraySize / chunkSize + 1;
     int blocks = chunks / THREADS_PER_BLOCK + 1;
-
-    gpu_mergeSort<<<blocks, THREADS_PER_BLOCK>>>(d_array, arraySize, chunkSize);
-    err = cudaDeviceSynchronize();
-    //printf("Merge Sort: %s\n", cudaGetErrorString(err));
+    gpu_sort<<<blocks, THREADS_PER_BLOCK>>>(d_array, arraySize, chunkSize);
+    cudaDeviceSynchronize();
 
     // Make temp array for the merge
+    cudaError_t err;
     int* d_temp_data;
     cudaMalloc((void **)&d_temp_data, arraySize*sizeof(int));
     while(chunkSize <= arraySize)
@@ -87,7 +98,7 @@ void mergeSort(int *array, int arraySize)
         blocks = chunks / THREADS_PER_BLOCK + 1;
         gpu_merge<<<blocks, THREADS_PER_BLOCK>>>(d_array, d_temp_data, arraySize, chunkSize);
         err = cudaDeviceSynchronize();
-        //printf("Merge: %s chunkSize: %d\n", cudaGetErrorString(err), chunkSize);
+        printf("Merge: %s chunkSize: %d\n", cudaGetErrorString(err), chunkSize);
     }
 
     // Copy result back to host
@@ -95,7 +106,8 @@ void mergeSort(int *array, int arraySize)
     cudaFree(d_array);
 }
 
-__global__ void gpu_mergeSort(int *d_array, int arraySize, int chunkSize)
+// sorts a bunch of small chunks from one big array
+__global__ void gpu_sort(int *d_array, int arraySize, int chunkSize)
 {
     // Figure out left and right for this thread
     int a = (threadIdx.x + blockDim.x * blockIdx.x) * chunkSize;
@@ -107,6 +119,7 @@ __global__ void gpu_mergeSort(int *d_array, int arraySize, int chunkSize)
     insertionSort(d_array, a, b);
 }
 
+// merges small sorted arrays into on big one
 __global__ void gpu_merge(int *d_array, int *d_temp_array, int arraySize, int chunkSize)
 {
     // Figure out left and right for this thread
@@ -117,11 +130,6 @@ __global__ void gpu_merge(int *d_array, int *d_temp_array, int arraySize, int ch
     int m = (b - a) / 2 + a;
     if (m >= arraySize) return;
     if (b > arraySize) b = arraySize;
-
-    // if (a == 0)
-    // {
-    //    printf("a:%d m:%d b:%d cs:%d\n", a, m, b, chunkSize);
-    // }
 
     int l = a;
     int r = m;
@@ -162,6 +170,7 @@ __global__ void gpu_merge(int *d_array, int *d_temp_array, int arraySize, int ch
     memcpy(d_array+a, d_temp_array+a, (b-a)*sizeof(int));
 }
 
+// sorts an array from [a,b)
 __device__ void insertionSort(int *array, int a, int b)
 {
     int current;
@@ -183,56 +192,8 @@ __device__ void insertionSort(int *array, int a, int b)
     }
 }
 
-__device__ void selectionSort(int *array, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        int smallest = array[i];
-        int index = i;
-        for (int j = i + 1; j < size; j++)
-        {
-            if (array[j] < smallest)
-            {
-                smallest = array[j];
-                index = j;
-            }
-        }
-        array[index] = array[i];
-        array[i] = smallest;
-    }
-}
-
-void insertionSortA(int *array, int a, int b)
-{
-    int current;
-    for (int i = a + 1; i < b; i++)
-    {
-        current = array[i];
-        for (int j = i - 1; j >= a - 1; j--)
-        {
-            if (j == a - 1 || current > array[j])
-            {
-                array[j + 1] = current;
-                break;
-            }
-            else
-            {
-                array[j + 1] = array[j];
-            }
-        }
-    }
-}
-
-void printArray(int *array, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-}
-
-__global__ void printGPUArray(int *d_array, int size)
+// prints an array
+__host__ __device__ void printArray(int *d_array, int size)
 {
     for (int i = 0; i < size; i++)
     {
@@ -241,8 +202,16 @@ __global__ void printGPUArray(int *d_array, int size)
     printf("\n");
 }
 
+// gets an array filled with random values
 int *getRandomArray(int size)
 {
+    // seed the prng if needed
+    if (randNotSeeded)
+    {
+        srand(time(0));
+        randNotSeeded = 0;
+    }
+
     int *array = (int *)malloc(size * sizeof(int));
     for (int i = 0; i < size; i++)
     {
@@ -251,7 +220,26 @@ int *getRandomArray(int size)
     return array;
 }
 
+// gets a random int in range [a,b)
 int randInt(int a, int b)
 {
     return (rand() % b) + a;
+}
+
+// used by qsort for comparisons
+int comparator(const void *p, const void *q)
+{
+    return *(const int *)p - *(const int *)q;
+}
+
+// returns true if success
+int compareArrays(int *array1, int *array2)
+{
+    for (int i = 0; i < SIZE; i++) {
+        if (array1[i] != array2[i]) {
+            printf("Broken at index:%d :(\n", i);
+            return false;
+        }
+    }
+    return true;
 }
--- cuda/sort.cuh
+++ cuda/sort.cuh
@@ -2,13 +2,13 @@
 #define SORT
 
 void mergeSort(int *array, int size);
-__global__ void gpu_mergeSort(int *d_array, int arraySize, int chunkSize);
+__global__ void gpu_sort(int *d_array, int arraySize, int chunkSize);
 __global__ void gpu_merge(int *d_array, int *d_temp_data, int arraySize, int chunkSize);
 __device__ void insertionSort(int *array, int a, int b);
-__device__ void selectionSort(int *array, int size);
-void printArray(int *array, int size);
-__global__ void printGPUArray(int *d_array, int size);
+__host__ __device__ void printArray(int *d_array, int size);
 int *getRandomArray(int size);
 int randInt(int a, int b);
+int comparator(const void *p, const void *q);
+int compareArrays(int *array1, int *array2);
 
 #endif
